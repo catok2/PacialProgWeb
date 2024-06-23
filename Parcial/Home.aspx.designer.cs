a6ddcd6321f2683d632f8f4c9dacf89c7a9ae204
Ailén <84881000+aailum@users.noreply.github.com>
2024-06-23 20:01:00 -0300

    Update Home.aspx.designer.cs

--- Parcial/Home.aspx.designer.cs
+++ Parcial/Home.aspx.designer.cs
@@ -22,5 +22,23 @@ namespace Parcial
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control btnIngreso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngreso;
+
+        /// <summary>
+        /// Control btnAdmin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAdmin;
     }
 }
